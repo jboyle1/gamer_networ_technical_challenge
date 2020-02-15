751d9404438d6c4f4f4676a08166435360b50df2
Jed Boyle <jboyle1@pca.ac.uk>
2020-02-15 16:32:56 +0000

    Created model database fields for Django admin in models.py. Then created a function in views.py to render the index.html file. I aslo created an app specific urls.py and configured all the url patterns, both in gamerNetPro1 and gameNetApp1.

--- gamerNetApp1/views.py
+++ gamerNetApp1/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from gamerNetApp1.models import Article
+
 
 # Create your views here.
+
+def index(request):
+    Article_list = Article.objects.order_by('title')
+    Article_dict = { 'Articles': Article_list}
+    return render(request, 'gamerNetApp1/index.html', context=Article_dict)
